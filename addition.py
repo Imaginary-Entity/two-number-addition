21c1cbbcdd418a9da016e55a62f328ff2b52c7ec
Imaginary-Entity <entityimaginary@gmail.com>
2016-09-24 12:35:26 +0100

    source code
    The main source code

---
+++ addition.py
@@ -0,0 +1,11 @@
+# Adds two numbers as input by user
+
+# Stores input numbers
+num1 = input('Enter a number: ')
+num2 = input(' Enter another number: ')
+
+#Addition
+sum = float(num1) + float(num2)
+
+# Display sum
+print(' The sum of {0} and {1} is {2}'. formet(num1, num2, sum))
